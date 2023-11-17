12923af9695dd2a879e95390033172305b91efd6
Erland Lie Amundgård <erlandla@stud.ntnu.no>
2023-11-17 11:09:55 +0100

    change test_size of train_test_split: 0.3 -> 0.2

--- random_forest_model.py
+++ random_forest_model.py
@@ -15,7 +15,7 @@ def random_forest(df, location):
     X = df.drop("consumption", axis=1)
 
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.3)
+        X, y, test_size=0.2)
 
     model = RandomForestRegressor(
         n_estimators=100,
